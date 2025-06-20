b241e0f9c90a1c9e9803ca1e7d1888dac20fb402
Maurizio Casti <maurizio.casti@aei.mpg.de>
2025-06-20 19:11:02 +0200

    Removes clean_vivado.bat

--- clean_vivado.bat
+++
@@ -1,8 +0,0 @@
-echo off                                          
-md Vivado_tmp                                     
-xcopy Vivado\*.xpr Vivado_tmp\*.xpr               
-rd /s /q Vivado                                   
-md Vivado                                         
-xcopy Vivado_tmp\*.xpr Vivado\*.xpr               
-rd /s /q Vivado_tmp                               
-echo "Finished: Vivado folder cleaned"            
